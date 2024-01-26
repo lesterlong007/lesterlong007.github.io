26b06d9109cec65a842d5f1da35c8b495e6c83f7
lesterlong007 <longchb13@lzu.edu.cn>
2024-01-26 23:19:04 +0800

    code rain set canvas size

--- js/rain.js
+++ js/rain.js
@@ -1,15 +1,18 @@
 const canvas = document.getElementById('rain');
-// const dpr = window.devicePixelRatio;
-const dpr = 1;
+const dpr = window.devicePixelRatio;
 
 const width = window.innerWidth * dpr;
 const height = window.innerHeight * dpr;
+canvas.width = width;
+canvas.height = height;
 
 const ctx = canvas.getContext('2d');
 const fontSize = 10 * dpr;
 const colWidth = fontSize;
 const colCount = Math.floor(width / colWidth);
 const nextChars = new Array(colCount).fill(0);
+
+ctx.scale(dpr, dpr);
 
 function getRandomColor() {
   const colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#800080', '#FFA500', '#FF4444', '#800000'];
@@ -22,18 +25,17 @@ function getRandomText() {
 }
 
 function paint() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < colCount; i++) {
     const char = getRandomText();
     ctx.fillStyle = getRandomColor();
-    ctx.font = `${fontSize}px serif`;
+    ctx.font = `${fontSize}px Arial`;
     const x = colWidth * i;
     const index = nextChars[i];
     const y = fontSize * (index + 1);
     ctx.fillText(char, x, y);
-    console.log(y, height);
-    if (y > height) {
+    if (y > height && Math.random() > 0.99) {
       nextChars[i] = 0;
     } else {
       nextChars[i]++;
@@ -41,13 +43,13 @@ function paint() {
   }
 }
 
-setInterval(() => {
+// setInterval(() => {
 //   paint();
-}, 40);
+// }, 40);
 
-// function runAnimation() {
-//   paint();
-//   requestAnimationFrame(runAnimation);
-// }
+function runAnimation() {
+  paint();
+  requestAnimationFrame(runAnimation);
+}
 
-// runAnimation();
+runAnimation();
