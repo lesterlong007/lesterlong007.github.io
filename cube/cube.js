0fc5a63a434e6d0118d354845789d88f4a6f6059
Lester <lester.long@prudential.com.cn>
2024-02-20 15:43:27 +0800

    cube rotate and undeveloped tooltips

--- cube/cube.js
+++ cube/cube.js
@@ -19,7 +19,7 @@ window.onload = function () {
 
   container.addEventListener('mousemove', function (event) {
     if (!isMouseDown) return;
-    const deltaX = event.clientX - startX ;
+    const deltaX = event.clientX - startX;
     const deltaY = event.clientY - startY;
     cube.style.transform = 'rotateX(' + (startRotateX - deltaY) + 'deg) rotateY(' + (startRotateY + deltaX) + 'deg)';
   });
@@ -34,29 +34,37 @@ window.onload = function () {
 
   const openBtn = document.querySelector('.footer .btn.open');
   const rotateBtn = document.querySelector('.footer .btn.rotate');
+  const spinBtn = document.querySelector('.footer .btn.spin');
+  const separateBtn = document.querySelector('.footer .btn.separate');
 
   let isOpen = false;
   let originalTransformMap = new Map();
-  openBtn.addEventListener('click', function() {
-    const frontFace = document.querySelector('.cube .face.front');
-    const backFace = document.querySelector('.cube .face.back');
-    const leftFace = document.querySelector('.cube .face.left');
-    const rightFace = document.querySelector('.cube .face.right');
-    const topFace = document.querySelector('.cube .face.top');
+  const frontFace = document.querySelector('.cube .face.front');
+  const backFace = document.querySelector('.cube .face.back');
+  const leftFace = document.querySelector('.cube .face.left');
+  const rightFace = document.querySelector('.cube .face.right');
+  const topFace = document.querySelector('.cube .face.top');
+  const bottomFace = document.querySelector('.cube .face.bottom');
+
+  openBtn.addEventListener('click', function () {
     if (isOpen) {
       frontFace.style.transform = originalTransformMap.get('front');
       backFace.style.transform = originalTransformMap.get('back');
       leftFace.style.transform = originalTransformMap.get('left');
       rightFace.style.transform = originalTransformMap.get('right');
       topFace.style.transform = originalTransformMap.get('top');
+      bottomFace.style.transform = originalTransformMap.get('bottom');
       openBtn.textContent = '打开外盒';
-    } else  {
+      isSeparated = false;
+      separateBtn.textContent = '拆分外盒';
+    } else {
       if (originalTransformMap.size === 0) {
         originalTransformMap.set('front', frontFace.style.transform);
         originalTransformMap.set('back', backFace.style.transform);
         originalTransformMap.set('left', leftFace.style.transform);
         originalTransformMap.set('right', rightFace.style.transform);
-        originalTransformMap.set('top', topFace.style.transform); 
+        originalTransformMap.set('top', topFace.style.transform);
+        originalTransformMap.set('bottom', bottomFace.style.transform);
       }
 
       frontFace.style.transform = 'rotateX(-90deg) rotateY(0deg) translateZ(150px) translateY(-290px)';
@@ -64,9 +72,10 @@ window.onload = function () {
       leftFace.style.transform = 'rotateX(-90deg) rotateY(0deg) translateZ(150px) translateX(-290px)';
       rightFace.style.transform = 'rotateZ(90deg) rotateY(90deg) translateZ(150px) translateY(-290px)';
       topFace.style.transform = 'rotateX(90deg) translateZ(250px)';
+      bottomFace.style.transform = 'rotateX(-90deg) translateZ(150px)';
       openBtn.textContent = '关闭外盒';
     }
-    
+
     isOpen = !isOpen;
   });
 
@@ -81,5 +90,52 @@ window.onload = function () {
     }
 
     isRotated = !isRotated;
+  });
+
+  let isSpin = false;
+  spinBtn.addEventListener('click', function () {
+    if (isSpin) {
+      cube.classList.remove('horizon-spin');
+      spinBtn.textContent = '水平旋转';
+    } else {
+      cube.classList.add('horizon-spin');
+      spinBtn.textContent = '停止旋转';
+    }
+
+    isSpin = !isSpin;
+  });
+
+  let isSeparated = false;
+  separateBtn.addEventListener('click', function () {
+    if (isSeparated) {
+      frontFace.style.transform = originalTransformMap.get('front');
+      backFace.style.transform = originalTransformMap.get('back');
+      leftFace.style.transform = originalTransformMap.get('left');
+      rightFace.style.transform = originalTransformMap.get('right');
+      topFace.style.transform = originalTransformMap.get('top');
+      bottomFace.style.transform = originalTransformMap.get('bottom');
+      separateBtn.textContent = '拆分外盒';
+      isOpen = false;
+      openBtn.textContent = '打开外盒';
+    } else {
+      if (originalTransformMap.size === 0) {
+        originalTransformMap.set('front', frontFace.style.transform);
+        originalTransformMap.set('back', backFace.style.transform);
+        originalTransformMap.set('left', leftFace.style.transform);
+        originalTransformMap.set('right', rightFace.style.transform);
+        originalTransformMap.set('top', topFace.style.transform);
+        originalTransformMap.set('bottom', bottomFace.style.transform);
+      }
+
+      frontFace.style.transform = 'translateZ(400px)';
+      backFace.style.transform = 'translateZ(-400px) rotateY(180deg)';
+      leftFace.style.transform = 'rotateY(-90deg) translateZ(400px)';
+      rightFace.style.transform = 'rotateY(90deg) translateZ(400px)';
+      topFace.style.transform = 'rotateX(90deg) translateZ(250px)';
+      bottomFace.style.transform = 'rotateX(-90deg) translateZ(400px)';
+      separateBtn.textContent = '聚拢外盒';
+    }
+
+    isSeparated = !isSeparated;
   });
 };
--- js/index.js
+++ js/index.js
@@ -56,125 +56,159 @@ const menuList = [
   }
 ];
 
-function renderMenuList() {
-  let htmlStr = '';
-  menuList.forEach(function (item, index) {
-    const { name = '', id } = item;
-    const curStr = `<div class="dock-item" data-name="${name}">\n<svg class="icon icon-${id}" aria-hidden="true">\n<use xlink:href="#icon-${id}" data-index="${index}" />\n</svg>\n</div>`;
-    htmlStr += curStr;
-    if (index < menuList.length - 1) {
-      htmlStr += `<span class="gap" data-index="${index}"></span>`;
-    }
-  });
-  document.querySelector('.dock').innerHTML = htmlStr;
-}
-
-renderMenuList();
-
-const menuEleList = document.querySelectorAll('.dock .dock-item');
-const gapList = document.querySelectorAll('.dock .gap');
-
-menuEleList.forEach(function (item) {
-  item.style.setProperty('--ratio', 1);
-});
-
-const dock = document.querySelector('.dock');
-const zoomCount = 4;
-const maxScale = 2;
-const step = 0.2;
-const menuLength = menuEleList.length;
-let curLabelEle = null;
-
-function removeLabel() {
-  if (curLabelEle) {
-    const parent = curLabelEle.parentNode;
-    if (parent) {
-      parent.removeChild(curLabelEle);
-    }
+const Toast = {
+  info(text, duration = 2, parentElement) {
+    const target = parentElement || document.body;
+    const toast = document.createElement('div');
+    toast.innerText = text;
+    toast.style.position = parentElement ? 'absolute' : 'fixed';
+    toast.style.left = '50%';
+    toast.style.top = '50%';
+    toast.style.maxWidth = '150px';
+    toast.style.lineHeight = '20px';
+    toast.style.transform = 'translate(-50%, -50%)';
+    toast.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    toast.style.padding = '8px 15px';
+    toast.style.borderRadius = '5px';
+    toast.style.fontSize = '16px';
+    toast.style.color = '#ffffff';
+    toast.style.zIndex = '99999';
+    target.appendChild(toast);
+    setTimeout(() => {
+      target.removeChild(toast);
+    }, duration * 1000);
+  },
+  success(text, duration = 2) {
+    this.info(text, duration);
+  },
+  fail(text, duration = 2) {
+    this.info(text, duration);
   }
-}
+};
 
-function showMenuName(ele, index) {
-  const menuName = menuList[index].name;
-  if (!menuName) {
-    return removeLabel();
+window.onload = function () {
+  function renderMenuList() {
+    let htmlStr = '';
+    menuList.forEach(function (item, index) {
+      const { name = '', id } = item;
+      const curStr = `<div class="dock-item" data-name="${name}">\n<svg class="icon icon-${id}" aria-hidden="true">\n<use xlink:href="#icon-${id}" data-index="${index}" />\n</svg>\n</div>`;
+      htmlStr += curStr;
+      if (index < menuList.length - 1) {
+        htmlStr += `<span class="gap" data-index="${index}"></span>`;
+      }
+    });
+    document.querySelector('.dock').innerHTML = htmlStr;
   }
-  if (curLabelEle) {
-    curLabelEle.textContent = menuName;
-  } else {
-    const label = document.createElement('span');
-    label.textContent = menuName;
-    label.style.fontSize = '16px';
-    label.style.position = 'absolute';
-    label.style.top = '-36px';
-    label.style.left = '50%';
-    label.style.whiteSpace = 'nowrap';
-    label.style.transform = 'translateX(-50%)';
-    curLabelEle = label;
-  }
-  if (!ele.contains(curLabelEle)) {
-    removeLabel();
-    ele.appendChild(curLabelEle);
-  }
-}
 
-function setScale(index) {
-  menuEleList.forEach(function (item, i) {
-    const distance = Math.abs(index - i);
-    if (distance === 0) {
-      item.style.setProperty('--ratio', maxScale);
-      showMenuName(item, i);
-    } else if (distance === 1) {
-      item.style.setProperty('--ratio', 1.8);
-    } else if (distance === 2) {
-      item.style.setProperty('--ratio', 1.5);
-    } else if (distance === 3) {
-      item.style.setProperty('--ratio', 1.1);
-    } else {
-      item.style.setProperty('--ratio', 1);
-    }
-  });
-}
+  renderMenuList();
 
-function resetScale() {
+  const menuEleList = document.querySelectorAll('.dock .dock-item');
+  const gapList = document.querySelectorAll('.dock .gap');
+
   menuEleList.forEach(function (item) {
     item.style.setProperty('--ratio', 1);
   });
-}
 
-dock.addEventListener('mouseover', function (e) {
-  const isContainer = e.target.classList.contains('dock');
-  const isGap = e.target.classList.contains('gap');
-  if (isContainer) {
-    resetScale();
-  } else if (isGap) {
-    const index = +e.target.dataset.index;
-    const { width = 20, left } = e.target.getBoundingClientRect();
-    if (e.clientX - left > width / 2) {
-      setScale(index + 1);
-    } else {
-      setScale(index);
+  const dock = document.querySelector('.dock');
+  const zoomCount = 4;
+  const maxScale = 2;
+  const step = 0.2;
+  const menuLength = menuEleList.length;
+  let curLabelEle = null;
+
+  function removeLabel() {
+    if (curLabelEle) {
+      const parent = curLabelEle.parentNode;
+      if (parent) {
+        parent.removeChild(curLabelEle);
+      }
     }
-  } else {
+  }
+
+  function showMenuName(ele, index) {
+    const menuName = menuList[index].name;
+    if (!menuName) {
+      return removeLabel();
+    }
+    if (curLabelEle) {
+      curLabelEle.textContent = menuName;
+    } else {
+      const label = document.createElement('span');
+      label.textContent = menuName;
+      label.style.fontSize = '16px';
+      label.style.position = 'absolute';
+      label.style.top = '-36px';
+      label.style.left = '50%';
+      label.style.whiteSpace = 'nowrap';
+      label.style.transform = 'translateX(-50%)';
+      curLabelEle = label;
+    }
+    if (!ele.contains(curLabelEle)) {
+      removeLabel();
+      ele.appendChild(curLabelEle);
+    }
+  }
+
+  function setScale(index) {
+    menuEleList.forEach(function (item, i) {
+      const distance = Math.abs(index - i);
+      if (distance === 0) {
+        item.style.setProperty('--ratio', maxScale);
+        showMenuName(item, i);
+      } else if (distance === 1) {
+        item.style.setProperty('--ratio', 1.8);
+      } else if (distance === 2) {
+        item.style.setProperty('--ratio', 1.5);
+      } else if (distance === 3) {
+        item.style.setProperty('--ratio', 1.1);
+      } else {
+        item.style.setProperty('--ratio', 1);
+      }
+    });
+  }
+
+  function resetScale() {
+    menuEleList.forEach(function (item) {
+      item.style.setProperty('--ratio', 1);
+    });
+  }
+
+  dock.addEventListener('mouseover', function (e) {
+    const isContainer = e.target.classList.contains('dock');
+    const isGap = e.target.classList.contains('gap');
+    if (isContainer) {
+      resetScale();
+    } else if (isGap) {
+      const index = +e.target.dataset.index;
+      const { width = 20, left } = e.target.getBoundingClientRect();
+      if (e.clientX - left > width / 2) {
+        setScale(index + 1);
+      } else {
+        setScale(index);
+      }
+    } else {
+      if (Object.hasOwnProperty.call(e.target.dataset, 'index')) {
+        const index = +e.target.dataset.index;
+        setScale([index]);
+      }
+    }
+  });
+
+  dock.addEventListener('mouseleave', function () {
+    resetScale();
+    removeLabel();
+    curLabelEle = null;
+  });
+
+  dock.addEventListener('click', function (e) {
     if (Object.hasOwnProperty.call(e.target.dataset, 'index')) {
       const index = +e.target.dataset.index;
-      setScale([index]);
+      const link = menuList[index].link;
+      if (link) {
+        window.location.assign(link);
+      } else {
+        Toast.info('暂未开发！');
+      }
     }
-  }
-});
-
-dock.addEventListener('mouseleave', function () {
-  resetScale();
-  removeLabel();
-  curLabelEle = null;
-});
-
-dock.addEventListener('click', function (e) {
-  if (Object.hasOwnProperty.call(e.target.dataset, 'index')) {
-    const index = +e.target.dataset.index;
-    const link = menuList[index].link;
-    if (link) {
-      window.location.assign(link);
-    }
-  }
-});
+  });
+};
